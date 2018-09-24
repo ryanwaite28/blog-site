7a33244cca9e5825998253fc06a1cc8cf9af65cf
Ryan M. Waite <ryanwaite28@gmail.com>
2018-09-24 10:58:06 -0400

    updated: added icons; removed user form

---
+++ array-sortby.js
@@ -0,0 +1,40 @@
+/*
+  https://github.com/ryanwaite28/script-store/blob/master/js/array-sortby.js
+  https://ryanwaite28.github.io/script-store/js/array-sortby.js
+
+  ---
+  Example
+  let people = [
+    { name: 'John', age: 0 },
+    { name: 'Jane', age: 0 },
+  ];
+
+  people.sortBy("name", "asc");
+*/
+
+Array.prototype.sortBy = function(property, direction) {
+  let tempArray = this;
+  tempArray.sort(function(a, b){
+    var x = a[property].constructor === String && a[property].toLowerCase() || a[property];
+    var y = b[property].constructor === String && b[property].toLowerCase() || b[property];
+    let value = direction && String(direction) || "asc";
+    switch(value) {
+      case "asc":
+        // asc
+        if (x < y) {return -1;}
+        if (x > y) {return 1;}
+        return 0;
+      case "desc":
+        // desc
+        if (x > y) {return -1;}
+        if (x < y) {return 1;}
+        return 0;
+      default:
+        // asc
+        if (x < y) {return -1;}
+        if (x > y) {return 1;}
+        return 0;
+    }
+  });
+  return tempArray;
+}
--- script.js
+++ script.js
@@ -2,243 +2,221 @@
 var App = angular.module("myApp", ["firebase"]);
 
 App.factory("bloggSite", ["$firebaseArray",
-	function($firebaseArray) {
-
-   	var ref = new Firebase("https://blogg-site.firebaseio.com/");
-
-   	// this uses AngularFire to create the synchronized array
+  function($firebaseArray) {
+    var ref = new Firebase("https://blogg-site.firebaseio.com/");
     return $firebaseArray(ref);
-	}
+  }
 ]);
 
 // Master Angular Controller
 App.controller("masterCtrl", ["$scope", "bloggSite",
-function($scope, bloggSite) {
+  function($scope, bloggSite) {
+		window.logApp = function(){ console.log($scope); }
 
-	$scope.blogSite = bloggSite;
+    $scope.blogSite = bloggSite;
+		$scope.blogSite.sortBy("name", "asc");
 
-	$scope.currentTopic = {};
-	$scope.currentTopicName = '';
-	$scope.currentTopicPosts = [];
-	$scope.currentid = '';
-	$scope.path = '';
+		$scope.user = null;
+		$scope.new_post_body = "";
 
-	$scope.hideForm = true;
+    $scope.currentTopic = {};
+    $scope.currentTopicName = '';
+    $scope.currentTopicPosts = [];
+    $scope.currentid = '';
+    $scope.path = '';
 
-	$('#topics').change(function(){
+    $scope.hideForm = true;
 
-		var value = $(this).val().toString();
-		console.log(value);
-
-		if(value == "-Select a Topic-") {
-			$scope.hideForm = true;
-		}
-		else {
-			$scope.hideForm = false;
-
-			var index = this.selectedIndex;
-			index = index - 1;
-			console.log(index);
-
-			$scope.currentTopic = bloggSite[index];
-			$scope.currentTopicName = bloggSite[index].name;
-			$scope.currentid = bloggSite[index].$id;
-			$scope.currentTopicPosts = bloggSite[index].posts;
-			console.log($scope.currentTopic);
-			console.log($scope.currentTopicPosts);
-
-		}
-
-		$scope.newPost = function(str) {
-			console.log(str);
-
-			var postName = $('#newpost-name').val();
-			var postBody = $('#newpost-body').val();
-
-			var date = Date();
-
-			if(postName == '' || postBody == '') {
-				alert('Please Fill In Both Fields.');
-				return;
-			}
-
-			var path = new Firebase('https://blogg-site.firebaseio.com/'+ $scope.currentid +'/posts');
-
-			if($scope.userPhoto != '' || $scope.userPhoto != null) {
-				path.push({
-					'personName' : postName,
-					'personThoughts' : postBody,
-					'postDate' : date,
-					'postImg' : $scope.userPhoto
-				})
-			}
+    $('#topics').change(function() {
+      var value = $(this).val();
+      if (value == "none") {
+        $scope.hideForm = true;
+      }
 			else {
-				path.push({
-					'personName' : postName,
-					'personThoughts' : postBody,
-					'postDate' : date
-				})
+        $scope.hideForm = false;
+				let blog_topic = bloggSite.filter(function(bt){ return bt.name === value })[0];
+				if(blog_topic) {
+					$scope.currentTopic = blog_topic;
+					$scope.currentTopicIndex = bloggSite.indexOf(blog_topic);
+	        $scope.currentTopicName = blog_topic.name;
+	        $scope.currentid = blog_topic.$id;
+	        $scope.currentTopicPosts = blog_topic.posts && Object.keys(blog_topic.posts).reduce(function(acc, cur){
+						return acc.concat(blog_topic.posts[cur])
+					}, []) || [];
+					$scope.$apply();
+				}
+      }
+    });
+
+		$scope.newPost = function() {
+			var date = Date();
+			if(!$scope.user) {
+				return alert('You must be logged in');
 			}
+			var path = new Firebase('https://blogg-site.firebaseio.com/' + $scope.currentid + '/posts');
+			var obj = {
+				'personName': $scope.user.name,
+				'personThoughts': $scope.new_post_body,
+				'postDate': date,
+				'postImg': $scope.user.picture
+			}
+			path.push(obj);
+
 
 			alert('Post Shared!');
-
-			$('#newpost-name').val($scope.userName);
-			$('#newpost-body').val('');
-
-			$scope.currentTopicPosts = bloggSite[index].posts;
-
-			//$scope.refreshBoard();
-
+			$scope.new_post_body = "";
+			$scope.currentTopicPosts = Object.keys($scope.blogSite[$scope.currentTopicIndex].posts).reduce(function(acc, cur){
+				return acc.concat($scope.blogSite[$scope.currentTopicIndex].posts[cur])
+			}, []);
 		}
 
-		$scope.refreshBoard = function() {
-			$scope.$apply(function(){
-				$scope.currentTopicPosts = bloggSite[index].posts;
-			})
-		}
+    $scope.signUp = function() {
 
-		$scope.$apply(function(){
+      //var name = $('#name').val().toLowerCase();
+      var emOne = $('#em-1').val().toLowerCase();
+      var emTwo = $('#em-2').val().toLowerCase();
+      var pwrdOne = $('#pwrd-1').val();
+      var pwrdTwo = $('#pwrd-2').val();
 
-		})
+      if (emOne == '' || emTwo == '' || pwrdOne == '' || pwrdTwo == '') {
+        alert('Please Fill In All The Fields.');
+        $('#em-1').val('');
+        $('#em-2').val('');
+        $('#pwrd-1').val('');
+        $('#pwrd-2').val('');
+        return;
+      } else if (emOne != emTwo || pwrdOne != pwrdTwo) {
+        alert("One of the two pairs (email or password) does not match.");
+        $('#em-1').val('');
+        $('#em-2').val('');
+        $('#pwrd-1').val('');
+        $('#pwrd-2').val('');
+        return;
+      } else {
+        // Continue Function
+        console.log("Sign Up Accepted.");
+      }
 
-	})
+      var ref = new Firebase("https://blogg-site.firebaseio.com");
+      ref.createUser({
+          userName: name,
+          email: emOne,
+          password: pwrdOne
+        },
+        function(error, userData) {
+          if (error) {
+            console.log("Error creating user:", error);
+          } else {
+            console.log("Successfully created user account with uid:", userData.uid);
+            alert('User Account Created! Use The Login Form to Log In!');
+            //$('#name').val('');
+            $('#em-1').val('');
+            $('#em-2').val('');
+            $('#pwrd-1').val('');
+            $('#pwrd-2').val('');
+          }
+        });
 
-	$scope.signUp = function() {
+    }
 
-		//var name = $('#name').val().toLowerCase();
-		var emOne = $('#em-1').val().toLowerCase();
-		var emTwo = $('#em-2').val().toLowerCase();
-		var pwrdOne = $('#pwrd-1').val();
-		var pwrdTwo = $('#pwrd-2').val();
+    $scope.logIn = function() {
 
-		if(emOne == '' || emTwo == '' || pwrdOne == '' || pwrdTwo == '') {
-			alert('Please Fill In All The Fields.');
-			$('#em-1').val('');
-			$('#em-2').val('');
-			$('#pwrd-1').val('');
-			$('#pwrd-2').val('');
-			return;
-		}
-		else if(emOne != emTwo || pwrdOne != pwrdTwo) {
-			alert("One of the two pairs (email or password) does not match.");
-			$('#em-1').val('');
-			$('#em-2').val('');
-			$('#pwrd-1').val('');
-			$('#pwrd-2').val('');
-			return;
-		}
-		else {
-			// Continue Function
-			console.log("Sign Up Accepted.");
-		}
+      var email = $('#email').val();
+      var password = $('#password').val();
 
-		var ref = new Firebase("https://blogg-site.firebaseio.com");
-		ref.createUser({
-			userName: name,
-  			email : emOne,
-  			password : pwrdOne
-		}, 
-		function(error, userData) {
-  			if (error) {
-    			console.log("Error creating user:", error);
-  			} else {
-    			console.log("Successfully created user account with uid:", userData.uid);
-  				alert('User Account Created! Use The Login Form to Log In!');
-  				//$('#name').val('');
-  				$('#em-1').val('');
-				$('#em-2').val('');
-				$('#pwrd-1').val('');
-				$('#pwrd-2').val('');
-  			}
-		});
+      var ref = new Firebase("https://blogg-site.firebaseio.com");
+      ref.authWithPassword({
+          email: email,
+          password: password
+        },
+        function(error, authData) {
+          if (error) {
+            console.log("Login Failed!", error);
+          } else {
+            console.log("Authenticated successfully with payload:", authData);
+            alert('User Login Successful! Hover Over The Menu Icon At The Top Right Corner To See Your Info!');
 
-	}
+            $scope.userName = authData.password.email;
+            $scope.userPhoto = authData.password.profileImageURL;
+            $scope.userLink = '#';
 
-	$scope.logIn = function() {
+            $('#newpost-name').val($scope.userName);
 
-		var email = $('#email').val();
-		var password = $('#password').val();
+            $scope.$apply(function() {})
 
-		var ref = new Firebase("https://blogg-site.firebaseio.com");
-		ref.authWithPassword({
-  			email    : email,
-  			password : password
-		},
-		function(error, authData) {
-  			if (error) {
-    			console.log("Login Failed!", error);
-  			} 
-  			else {
-    			console.log("Authenticated successfully with payload:", authData);
-  				alert('User Login Successful! Hover Over The Menu Icon At The Top Right Corner To See Your Info!');
+          }
+        });
 
-    			$scope.userName = authData.password.email;
-    			$scope.userPhoto = authData.password.profileImageURL;
-    			$scope.userLink = '#';
+    }
 
-    			$('#newpost-name').val($scope.userName);
+    $scope.fbLogin = function() {
+      var ref = new Firebase("https://blogg-site.firebaseio.com");
+      ref.authWithOAuthPopup("facebook", function(error, authData) {
+        if (error) {
+          console.log("Login Failed!", error);
+          console.log("Login Failed.");
+        } else {
+          console.log("Authenticated successfully with payload:", authData);
+          alert('User Login Successful! Hover Over The Menu Icon At The Top Right Corner To See Your Info!');
 
-    			$scope.$apply(function(){})
+          $scope.userName = authData.facebook.displayName;
+          $scope.userPhoto = authData.facebook.profileImageURL;
+          $scope.userLink = authData.facebook.cachedUserProfile.link;
 
-  			}
-		});
+          $('#newpost-name').val($scope.userName);
 
-	}
+          $scope.$apply(function() {})
 
-	$scope.fbLogin = function() {
+        }
+      });
+    }
 
-		var ref = new Firebase("https://blogg-site.firebaseio.com");
-		ref.authWithOAuthPopup("facebook", function(error, authData) {
-  			if (error) {
-    			console.log("Login Failed!", error);
-    			console.log("Login Failed.");
-  			} 
-  			else {
-    			console.log("Authenticated successfully with payload:", authData);
-    			alert('User Login Successful! Hover Over The Menu Icon At The Top Right Corner To See Your Info!');
+		$scope.gLogin = function() {
+      var ref = new Firebase("https://blogg-site.firebaseio.com");
+      ref.authWithOAuthPopup("google", function(error, authData) {
+        if (error) {
+          console.log("Login Failed!", error);
+        } else {
+          console.log("Authenticated successfully with payload:", authData);
+					$scope.user = authData.google.cachedUserProfile;
+					$scope.user.uid = authData.uid;
+					$scope.$apply();
+					alert('Logged In!');
+        }
+      });
+    }
 
-    			$scope.userName = authData.facebook.displayName;
-    			$scope.userPhoto = authData.facebook.profileImageURL;
-    			$scope.userLink = authData.facebook.cachedUserProfile.link;
-
-    			$('#newpost-name').val($scope.userName);
-
-    			$scope.$apply(function(){})
-
-  			}
-		});
-	}
-
-	$scope.newTopic = function() {
-		console.log("New Topic.");
-
-		var newTopic = $('#new-topic').val().toLowerCase();
-
-		if(newTopic == '') {
-			alert('Please Enter a Name for a New Topic.');
-			return;
-		}
-
-		for(var key in $scope.blogSite) {
-
-			if(newTopic == $scope.blogSite[key].name){
-				alert('Topic Already Exists. Select It From The DropDown.');
-				console.log('New Topic Rejected.');
-				return;
+    $scope.newTopic = function() {
+      if(!$scope.user) {
+				return alert('You must be logged in');
 			}
 
-		}
+      var newTopic = $('#new-topic').val().toLowerCase();
 
-		$scope.blogSite.$add({
-			name : newTopic,
-			//date : Date(),
-			//posts : ['']
-		})
-		console.log('New Topic Accepted.');
-		alert("New Topic Created!");
-		var newTopic = $('#new-topic').val('');
+      if (newTopic == '') {
+        alert('Please Enter a Name for a New Topic.');
+        return;
+      }
 
-	}
+      for (var key in $scope.blogSite) {
 
-}
+        if (newTopic == $scope.blogSite[key].name) {
+          alert('Topic Already Exists. Select It From The DropDown.');
+          console.log('New Topic Rejected.');
+          return;
+        }
+
+      }
+
+      $scope.blogSite.$add({
+        name: newTopic
+      });
+			$scope.blogSite.sortBy("name", "asc");
+			$scope.$apply();
+
+      var newTopic = $('#new-topic').val('');
+
+    }
+
+  }
 ]);
